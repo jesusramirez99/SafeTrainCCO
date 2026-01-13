cb82a3f93c6cabcf5106494368932ce7c28d8f7f
Jesus <124712591+jesusramirez99@users.noreply.github.com>
2026-01-13 09:44:33 -0600

    UPDATE enviroments

--- lib/config/enviroments.dart
+++ lib/config/enviroments.dart
@@ -1,7 +1,16 @@
-import 'dart:js' as js;
+import 'dart:js_interop';
+
+@JS('env')
+external EnvJS get _env;
+
+@JS()
+@anonymous
+class EnvJS{
+  external String get BASE_URL;
+  external String get BASE_URL1;
+}
 
 class Enviroment {
-
-  static String get baseUrl =>
-      js.context['env']['BASE_URL'];
+  static String get baseUrl => _env.BASE_URL;
+  static String get baseUrlDev => _env.BASE_URL1;
 }
